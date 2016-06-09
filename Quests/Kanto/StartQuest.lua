3dd859b94388c692e5233b7767c80b47aab4f872
g0ld <g0ld@tuta.io>
2016-06-09 16:36:05 +0200

    Add missing StartQuest.lua

---
+++ Quests/Kanto/StartQuest.lua
@@ -0,0 +1,29 @@
+-- Copyright © 2016 g0ld <g0ld@tuta.io>
+-- This work is free. You can redistribute it and/or modify it under the
+-- terms of the Do What The Fuck You Want To Public License, Version 2,
+-- as published by Sam Hocevar. See the COPYING file for more details.
+
+local sys    = require "Libs/syslib"
+local Quest  = require "Quests/Quest"
+local Dialog = require "Quests/Dialog"
+
+local name        = 'Start'
+local description = 'Play the Start map'
+
+local StartQuest = Quest:new()
+function StartQuest:new()
+	return Quest.new(StartQuest, name, description, dialogs)
+end
+
+function StartQuest:isDoable()
+	return getMapName() == "Start"
+end
+
+function StartQuest:Start()
+	if isNpcOnCell(21,38) then
+		return talkToNpcOnCell(21,38)
+	end
+	return moveToCell(26,87)
+end
+
+return StartQuest
