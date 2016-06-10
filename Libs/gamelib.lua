57e55a6b90b5c49df2b815c4b53e4f450cfdcee9
g0ld <g0ld@tuta.io>
2016-06-10 17:34:51 +0200

    Add gamelib

---
+++ Libs/gamelib.lua
@@ -0,0 +1,62 @@
+-- Copyright © 2016 g0ld <g0ld@tuta.io>
+-- This work is free. You can redistribute it and/or modify it under the
+-- terms of the Do What The Fuck You Want To Public License, Version 2,
+-- as published by Sam Hocevar. See the COPYING file for more details.
+
+local game = {}
+
+local sys = require("Libs/syslib")
+
+function game.isTeamFullyHealed()
+	for pokemonId=1, getTeamSize(), 1 do
+		if getPokemonHealthPercent(pokemonId) < 100
+			or not isPokemonUsable(pokemonId) then
+			return false
+		end
+	end
+	return true
+end
+
+function game.isPokemonFullPP(pokemonId)
+	sys.todo("add getPokemonMoves() to PROShine")
+	error("waiting for proshine update")
+	return true
+end
+
+function game.minTeamLevel()
+	local current
+	for pokemonId=1, getTeamSize(), 1 do
+		local pokemonLevel = getPokemonLevel(pokemonId)
+		if  current == nil or pokemonLevel < current then
+			current = pokemonLevel
+		end
+	end
+	return current
+end
+
+function game.maxTeamLevel()
+	local current
+	for pokemonId=1, getTeamSize(), 1 do
+		local pokemonLevel = getPokemonLevel(pokemonId)
+		if  current == nil or pokemonLevel > current then
+			current = pokemonLevel
+		end
+	end
+	return current
+end
+
+function game.getMaxLevelUsablePokemon()
+	local currentId
+	local currentLevel
+	for pokemonId=1, getTeamSize(), 1 do
+		local pokemonLevel = getPokemonLevel(pokemonId)
+		if  (currentLevel == nil or pokemonLevel > currentLevel)
+			and isPokemonUsable(pokemonId) then
+			currentLevel = pokemonLevel
+			currentId    = pokemonId
+		end
+	end
+	return currentId, currentLevel
+end
+
+return game
