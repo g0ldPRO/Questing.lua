a279adb7aa9fd38b46560875ebf7e77b1b07c21a
g0ld <g0ld@tuta.io>
2016-06-11 18:55:45 +0200

    syslib: Add tableHasValue

--- Libs/syslib.lua
+++ Libs/syslib.lua
@@ -22,7 +22,16 @@ function sys.stringContains(haystack, needle)
 end
 
 function sys.removeCharacter(haystack, char)
-	return haystack:gsub(char .. "+", "")
+	return haystack:gsub("%" .. char .. "+", "")
+end
+
+function sys.tableHasValue(tab, val)
+    for index, value in ipairs(tab) do
+        if value == val then
+            return true
+        end
+    end
+    return false
 end
 
 return sys
