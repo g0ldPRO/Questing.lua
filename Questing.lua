351841f824ea00bcd804267658137779efc7b33e
g0ld <g0ld@tuta.io>
2016-06-12 03:14:35 +0200

    Add useless action in case of server issue

--- Questing.lua
+++ Questing.lua
@@ -10,7 +10,7 @@ description = [[Everything.]]
 dofile "config.lua"
 
 local QuestManager
-local questManager
+local questManager = nil
 
 function onStart()
 	math.randomseed(os.time())
@@ -29,7 +29,11 @@ function onStop()
 end
 
 function onPathAction()
-	questManager:path()
+	if questManager:path() == false then
+		-- may be a bug (like NPC not loaded yet)
+		log("No action found in quest: the server may be lagging. Using placeholder action.")
+		return swapPokemon(1,2) or moveNearExit() or moveToGrass()
+	end
 	if questManager.isOver then
 		return fatal("No more quest to do. Script terminated.")
 	end
