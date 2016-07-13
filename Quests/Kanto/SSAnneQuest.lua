f10a4ccb53b0b1e6394c86bee858745d939b9024
g0ld <g0ld@tuta.io>
2016-07-13 02:43:16 +0200

    Review SSAneQuest

--- Quests/Kanto/SSAnneQuest.lua
+++ Quests/Kanto/SSAnneQuest.lua
@@ -1,28 +1,20 @@
--- Copyright © 2016 g0ld <g0ld@tuta.io>
+-- Copyright © 2016 Rympex <Rympex@noemail>
 -- This work is free. You can redistribute it and/or modify it under the
 -- terms of the Do What The Fuck You Want To Public License, Version 2,
 -- as published by Sam Hocevar. See the COPYING file for more details.
--- Quest: By Rympe 
 
 local sys    = require "Libs/syslib"
 local game   = require "Libs/gamelib"
 local Quest  = require "Quests/Quest"
 local Dialog = require "Quests/Dialog"
 
-local name        = 'SS-Anne Quest: '
-local description = 'Route 5 to Vermilion City'
+local name        = 'SSAnne Quest'
+local description = 'Completes the SSAnne'
 local level       = 26
-
-local TrashBin1Kitchen = false
-local TrashBin2Kitchen = false
-local TrashBin3Kitchen = false
-local TrashBin1Ballroom = false
-local TrashBin2Ballroom = false
-local TrashBin3Ballroom = false
-local TrashBinLeftovers = false
 
 local dialogs = {
 	NeedRegisterTicket = Dialog:new({ 
+		"has been booked in the passenger registry of S.S. Anne!",
 		"pc is already registered to your"
 	}),
 	PcGetRegisterDone = Dialog:new({ 
@@ -52,18 +44,7 @@ function SSAnneQuest:new()
 end
 
 function SSAnneQuest:isDoable()
-	if self:hasMap() then
-		return true
-	end
-	return false
-end
-
-function SSAnneQuest:isDone()
-	if hasItem("HM01 - Cut") and getMapName() == "Vermilion City" then
-		return true
-	else
-		return false
-	end
+	return self:hasMap()
 end
 
 function SSAnneQuest:SSAnneBasement()
@@ -88,28 +69,28 @@ end
 
 function SSAnneQuest:BallroomSSAnne()
 	if isNpcOnCell(2,20) then
-		if isNpcOnCell(20, 33) and not TrashBin1Ballroom then -- Item: Leppa Berry			
+		if isNpcOnCell(20, 33) and not self.trashBin1Ballroom then -- Item: Leppa Berry
 			if not dialogs.TrashBinCheck.state then
 				return talkToNpcOnCell(20, 33)
 			else
 				dialogs.TrashBinCheck.state = false
-				TrashBin1Ballroom = true
+				self.trashBin1Ballroom = true
 				return
 			end
-		elseif isNpcOnCell(10, 33) and not TrashBin2Ballroom then -- Item: Chesto Berry
+		elseif isNpcOnCell(10, 33) and not self.trashBin2Ballroom then -- Item: Chesto Berry
 			if not dialogs.TrashBinCheck.state then
 				return talkToNpcOnCell(10, 33)
 			else
 				dialogs.TrashBinCheck.state = false
-				TrashBin2Ballroom = true
+				self.trashBin2Ballroom = true
 				return
 			end		
-		elseif isNpcOnCell(23, 39) and not TrashBin3Ballroom then -- Item: Oran Berry
+		elseif isNpcOnCell(23, 39) and not self.trashBin3Ballroom then -- Item: Oran Berry
 			if not dialogs.TrashBinCheck.state then
 				return talkToNpcOnCell(23, 39)
 			else
 				dialogs.TrashBinCheck.state = false
-				TrashBin3Ballroom = true
+				self.trashBin3Ballroom = true
 				return
 			end
 		else
@@ -139,14 +120,14 @@ function SSAnneQuest:SSAnne2F()
 		end
 	elseif not hasItem("HM01 - Cut") then
 		if dialogs.PharmacistWorking.state then
-			return moveToMap("SSAnne 3F")				
+			return moveToMap("SSAnne 3F")
 		else
-			if isNpcOnCell(28, 18) and not TrashBinLeftovers then -- Item: LeftOvers
+			if isNpcOnCell(28, 18) and not self.trashBinLeftovers then -- Item: LeftOvers
 				if not dialogs.TrashBinCheck.state then
 					return talkToNpcOnCell(28, 18)
 				else
 					dialogs.TrashBinCheck.state = false
-					TrashBinLeftovers = true
+					self.trashBinLeftovers = true
 					return
 				end
 			else
@@ -179,31 +160,30 @@ end
 function SSAnneQuest:SSAnne1FKitchen()
 	if dialogs.NeedPharmacist.state or dialogs.KitchenDone.state then
 		return moveToMap("SSAnne 1F")
-	--elseif then	
 	else
 		-- GET ITEMS CHECK
-		if isNpcOnCell(14, 7) and not TrashBin1Kitchen then -- Item: Great Ball			
+		if isNpcOnCell(14, 7) and not self.trashBin1Kitchen then -- Item: Great Ball
 			if not dialogs.TrashBinCheck.state then
 				return talkToNpcOnCell(14, 7)
 			else
 				dialogs.TrashBinCheck.state = false
-				TrashBin1Kitchen = true
+				self.trashBin1Kitchen = true
 				return
 			end
-		elseif isNpcOnCell(14, 9) and not TrashBin2Kitchen then -- Item: Pecha Berry - [Mission Pharmacist]
+		elseif isNpcOnCell(14, 9) and not self.trashBin2Kitchen then -- Item: Pecha Berry - [Mission Pharmacist]
 			if not dialogs.TrashBinCheck.state then
 				return talkToNpcOnCell(14, 9)
 			else
 				dialogs.TrashBinCheck.state = false
-				TrashBin2Kitchen = true
+				self.trashBin2Kitchen = true
 				return
-			end		
-		elseif isNpcOnCell(14, 11) and not TrashBin3Kitchen then -- Item: Hyper Potion
+			end
+		elseif isNpcOnCell(14, 11) and not self.trashBin3Kitchen then -- Item: Hyper Potion
 			if not dialogs.TrashBinCheck.state then
 				return talkToNpcOnCell(14, 11)
 			else
 				dialogs.TrashBinCheck.state = false
-				TrashBin3Kitchen = true
+				self.trashBin3Kitchen = true
 				return
 			end
 		else
