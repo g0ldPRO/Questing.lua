07d08aa92265067516f3ec584e06b8b708810e1b
Rympex <rymperius@gmail.com>
2016-07-18 13:07:20 +0200

    Update config.lua
    + Removed BUY_BIKE

--- config.lua
+++ config.lua
@@ -1,4 +1,3 @@
 KANTO_STARTER_ID=math.random(1,4) -- 1: Fatvegan, 2: Salamender, 3: Aquaturtle, 4: Mickeychu
 KANTO_FOSSIL_ID =math.random(1,2) -- 1: Helix, 2: Dome
 DOJO_POKEMON_ID =math.random(1,2) -- 1: Hitmonchan, 2: Hitmonlee
-BUY_BYKE =true
